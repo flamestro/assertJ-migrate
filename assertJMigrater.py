d0fd1e86115d281cb52d1acfb450f3a4306a1ac0
flamestro <19ahmet99@gmail.com>
2020-03-12 18:17:38 +0100

    implemented migration script, added README.MD

---
+++ assertJMigrater.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""assertJMigrater.py: This is an script to easily migrate JUnit Assert to AssertJ"""
+
+__author__ = "Ahmet Kilic https://github.com/flamestro"
+
+import fileinput
+import os
+import sys
+
+
+def substring_maker(inputstring, start, end, index=0):
+    return (inputstring.split(start))[-1].split(end)[index]
+
+
+file_path = input("Please enter the absolute path to the file you want to migrate :")
+file_name = file_path.rsplit("/", 1)[1]
+file_dir = file_path.rsplit("/", 1)[0]
+print(file_dir, file_name, file_path)
+os.system("cd {}".format(file_dir))
+for line in fileinput.input(file_name, inplace=True):
+    next_line = False
+    start_line = line
+    if not line.endswith(";"):
+        try:
+            line = line.split(";")[0] + ";"
+        except:
+            sys.stdout.write(line)
+            continue
+        next_line = True
+    if "assertEquals" in line and line.endswith(";"):
+        parameters = substring_maker(line, "assertEquals(", ");")
+        actual = parameters.split(",")[1]
+        expected = parameters.split(",")[0]
+        if actual.startswith(" "):
+            actual = actual[1:]
+        new_line = "assertThat({}).isEqualTo({});\n".format(actual, expected)
+    elif "assertTrue" in line and line.endswith(";"):
+        parameter = substring_maker(line, "assertTrue(", ");")
+        actual = parameter
+        if actual.startswith(" "):
+            actual = actual[1:]
+        new_line = "assertThat({}).isTrue();\n".format(actual)
+    elif "assertFalse" in line and line.endswith(";"):
+        parameter = substring_maker(line, "assertFalse(", ");")
+        actual = parameter
+        if actual.startswith(" "):
+            actual = actual[1:]
+        new_line = "assertThat({}).isFalse();\n".format(actual)
+    elif "assertNull" in line and line.endswith(";"):
+        parameter = substring_maker(line, "assertNull(", ");")
+        actual = parameter
+        if actual.startswith(" "):
+            actual = actual[1:]
+        new_line = "assertThat({}).isNull();\n".format(actual)
+    elif "assertNotNull" in line and line.endswith(";"):
+        parameter = substring_maker(line, "assertNotNull(", ");")
+        actual = parameter
+        if actual.startswith(" "):
+            actual = actual[1:]
+        new_line = "assertThat({}).isNotNull();\n".format(actual)
+    else:
+        new_line = start_line
+    sys.stdout.write(new_line)
