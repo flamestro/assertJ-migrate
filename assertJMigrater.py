02b4e59eabc02cb4366e17a5063e1d4a8f3d36ff
flamestro <19ahmet99@gmail.com>
2020-04-03 17:47:11 +0200

    fix bug with imports causing problems

--- assertJMigrater.py
+++ assertJMigrater.py
@@ -14,10 +14,15 @@ def substring_maker(inputstring, start, end, index=0):
 
 
 file_path = input("Please enter the absolute path to the file you want to migrate :")
-file_name = file_path.rsplit("/", 1)[1]
-file_dir = file_path.rsplit("/", 1)[0]
-print(file_dir, file_name, file_path)
-os.system("cd {}".format(file_dir))
+try:
+    file_name = file_path.rsplit("/", 1)[1]
+    file_dir = file_path.rsplit("/", 1)[0]
+    print(file_dir, file_name, file_path)
+    os.system("cd {}".format(file_dir))
+except:
+    file_name = file_path
+    file_dir = file_path
+
 for line in fileinput.input(file_name, inplace=True):
     start_line = line
     if not line.endswith(";"):
@@ -26,32 +31,32 @@ for line in fileinput.input(file_name, inplace=True):
         else:
             sys.stdout.write(line)
             continue
-    if "assertEquals" in line and line.endswith(";"):
+    if "assertEquals" in line and line.endswith(";") and ".assertEquals" not in line:
         parameters = substring_maker(line, "assertEquals(", ");")
         actual = parameters.split(",")[1]
         expected = parameters.split(",")[0]
         if actual.startswith(" "):
             actual = actual[1:]
         new_line = "assertThat({}).isEqualTo({});\n".format(actual, expected)
-    elif "assertTrue" in line and line.endswith(";"):
+    elif "assertTrue" in line and line.endswith(";") and ".assertTrue" not in line:
         parameter = substring_maker(line, "assertTrue(", ");")
         actual = parameter
         if actual.startswith(" "):
             actual = actual[1:]
         new_line = "assertThat({}).isTrue();\n".format(actual)
-    elif "assertFalse" in line and line.endswith(";"):
+    elif "assertFalse" in line and line.endswith(";") and ".assertFalse" not in line:
         parameter = substring_maker(line, "assertFalse(", ");")
         actual = parameter
         if actual.startswith(" "):
             actual = actual[1:]
         new_line = "assertThat({}).isFalse();\n".format(actual)
-    elif "assertNull" in line and line.endswith(";"):
+    elif "assertNull" in line and line.endswith(";") and ".assertNull" not in line:
         parameter = substring_maker(line, "assertNull(", ");")
         actual = parameter
         if actual.startswith(" "):
             actual = actual[1:]
         new_line = "assertThat({}).isNull();\n".format(actual)
-    elif "assertNotNull" in line and line.endswith(";"):
+    elif "assertNotNull" in line and line.endswith(";") and ".assertNotNull" not in line:
         parameter = substring_maker(line, "assertNotNull(", ");")
         actual = parameter
         if actual.startswith(" "):
